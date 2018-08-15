4d41c082adb76da5f83aa271037ef9681d53ac0f
Neven M <nevemark@gmail.com>
2018-08-15 20:25:27 +0200

    First commit

---
+++ 8kyu_multiply.js
@@ -0,0 +1,6 @@
+// 8 kyu - Multiply
+
+// This function has to be called multiply and needs to take two numbers as 
+// arguments, and has to return the multiplication of the two arguments.
+
+let multiply = (a,b) => a * b;
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# CodeWars
-Repository of my code challenges on CodeWars.
+﻿# CodeWars code challenges
+
+
+
+
+This is a repository of my [CodeWars](https://www.codewars.com/) coding challenges.
