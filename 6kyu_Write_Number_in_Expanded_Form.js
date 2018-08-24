d990792b5badd25c32a564969bddd56601d124d7
Neven M <nevemark@gmail.com>
2018-08-24 18:04:46 +0200

    6kyu_Write_Number_in_Expanded_Form.js

---
+++ 6kyu_Write_Number_in_Expanded_Form.js
@@ -0,0 +1,23 @@
+// 6 kyu - Write Number in Expanded Form
+
+// Write Number in Expanded Form
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+// NOTE: All numbers will be whole numbers greater than 0.
+
+// If you liked this kata, check out part 2!!
+
+function expandedForm(num) {
+  var result = '';
+  var numArr = (''+num).split('');
+  for (var i = 0; i < numArr.length; i++) {
+    if (numArr[i] !== '0') {
+      result = result + numArr[i] + "0".repeat(numArr.length - i - 1) + ' + '
+    }
+  }
+  result = result.split('').slice(0, -3).join(''); 
+  return result;
+}
